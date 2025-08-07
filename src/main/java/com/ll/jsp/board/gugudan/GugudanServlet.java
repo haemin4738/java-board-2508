d7c3954894c744d9e4c1a27c1ce71d1c11d1e61b
haemin4738 <leeheamin12@gmail.com>
2025-08-07 12:49:21 +0900

    구구단

--- src/main/java/com/ll/jsp/board/gugudan/GugudanServlet.java
+++ src/main/java/com/ll/jsp/board/gugudan/GugudanServlet.java
@@ -1,4 +1,4 @@
-package com.ll.jsp.board.servlet;
+package com.ll.jsp.board.gugudan;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,16 +8,22 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
 
-        resp.getWriter().append("<h1>Hello, World!@!@!@!</h1>");
 
-
+        String danStr = req.getParameter("dan");
+        String limitStr = req.getParameter("limit");
+        int dan = Integer.parseInt((danStr));
+        int limit = Integer.parseInt(limitStr);
+        resp.getWriter().append("<h1>== %s 단 ==</h1>".formatted(danStr));
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d = %d</div>".formatted(dan, i, dan * i));
+        }
     }
 }
