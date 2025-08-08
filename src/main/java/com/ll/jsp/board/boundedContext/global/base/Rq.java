5be61ec24fb3b95abe9b1b893c47f88db0375264
haemin4738 <leeheamin12@gmail.com>
2025-08-08 11:13:28 +0900

    EL 사용

--- src/main/java/com/ll/jsp/board/boundedContext/global/base/Rq.java
+++ src/main/java/com/ll/jsp/board/boundedContext/global/base/Rq.java
@@ -1,10 +1,14 @@
 package com.ll.jsp.board.boundedContext.global.base;
 
+import com.ll.jsp.board.boundedContext.article.dto.Article;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class Rq {
     private final HttpServletRequest req;
@@ -55,5 +59,26 @@ public class Rq {
         } catch (IOException e) {
             throw new RuntimeException("응답 작성 중 오류가 발생했습니다.", e);
         }
+    }
+
+    public void view(String path) {
+        //"/jsp/usr/article/list.jsp" 이런형태로 만들어줌
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/jsp/" + path + ".jsp");
+
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException e) {
+            throw new RuntimeException("뷰로의 포워딩 중 오류가 발생했습니다.", e);
+        } catch (IOException e) {
+            throw new RuntimeException("뷰로의 포워딩 중 IO 오류가 발생했습니다.", e);
+        }
+    }
+
+    public void getAttr(String name) {
+        req.getAttribute(name);
+    }
+
+    public void setAttr(String name, Object value) {
+        req.setAttribute(name, value);
     }
 }
---
+++ src/webapp/jsp/usr/article/list.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@ page import="com.ll.jsp.board.boundedContext.article.dto.Article" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.List" %>
+
+<%
+List<Article> articleList = (List<Article>) request.getAttribute("articleList");
+    %>
+
+    <h1>게시물 리스트</h1>
+
+    <div>
+        <ul>
+            <% for (Article article : articleList) { %>
+            <li><%= article.getId() %>번 : <%= article.getTitle() %></li>
+            <% } %>
+        </ul>
+        <ul>
+        <%
+        for (int i = articleList.size() - 1; i >= 0; i--) {
+        pageContext.setAttribute("article", articleList.get(i));
+        %>
+        <li>${article.id}번 : ${article.title}</li>
+        <% } %>
+        </ul>
+    </div>
