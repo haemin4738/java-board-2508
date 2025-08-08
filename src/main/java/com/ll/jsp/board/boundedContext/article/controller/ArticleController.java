b5480a56422b2fcbeea1de803830e3b4bca1caf9
haemin4738 <leeheamin12@gmail.com>
2025-08-08 11:35:29 +0900

    역순 출력 코드 작성

--- src/main/java/com/ll/jsp/board/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/ll/jsp/board/boundedContext/article/controller/ArticleController.java
@@ -4,9 +4,9 @@ package com.ll.jsp.board.boundedContext.article.controller;
 import com.ll.jsp.board.boundedContext.article.dto.Article;
 import com.ll.jsp.board.boundedContext.global.base.Rq;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class ArticleController {
     private List<Article> articleList;
@@ -24,8 +24,13 @@ public class ArticleController {
     }
 
     public void showList(Rq rq) {
-        rq.setAttr("articleList", articleList);
+//        Stream
+//        List<Article> articleList = this.articleList.stream()
+//                .sorted(Comparator.comparing(Article::getId).reversed()) // 정렬 기준 예시
+//                .toList();
 
+        Collections.reverse(articleList);
+        rq.setAttr("articleList", articleList);
         rq.view("usr/article/list");
     }
 
